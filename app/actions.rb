8fdc4689d9e0eb032c0342358f23f7cc1472a3e6
LeahChurchley <vagrant@lighthouse-labs>
2016-04-19 02:17:54 +0000

    now when you click on a song, it goes to an info page about the song

--- app/actions.rb
+++ app/actions.rb
@@ -12,3 +12,19 @@ get '/songs/new' do
   @song = Song.new
   erb :'songs/new'
 end
+
+post '/songs' do
+  @song = Song.new(
+    title: params[:title],
+    author: params[:author],
+    url: params[:url]
+  )
+  @song.save
+  redirect '/songs'
+
+end
+
+get '/songs/:id' do 
+  @song = Song.find params[:id]
+  erb :'songs/show'
+end
